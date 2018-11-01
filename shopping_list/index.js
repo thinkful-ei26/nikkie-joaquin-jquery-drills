750bdf200f8b55103abb0eb8dc99d20a3b373fde
Nikkie Mashian <nikmash@ucla.edu>
2018-11-01 12:29:41 -0700

    started the shopping list challenge

---
+++ shopping_list/index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+function generateListItem(item) //does this also need to be wrapped in a jquery object?
+{
+  return `<li>
+  <span class="shopping-item">${item}</span>
+  <div class="shopping-item-controls">
+    <button class="shopping-item-toggle">
+      <span class="button-label">check</span>
+    </button>
+    <button class="shopping-item-delete">
+      <span class="button-label">delete</span>
+    </button>
+  </div>
+</li>`;
+}
+
+$(function(){
+  $('form').submit((event)=>{
+    event.preventDefault(); //since we're submitting we need to prevent the default 
+
+    //grab what was inputted
+    const newItem = $('form input').val();
+    // console.log(newItem);
+
+    //clear the input box
+    event.target.reset();
+
+    //make a list element with the input
+    const listElement = generateListItem(newItem);
+    console.log(listElement); 
+
+    //make that list element appear
+    $('.shopping-list').append(listElement);
+
+  });
+});
