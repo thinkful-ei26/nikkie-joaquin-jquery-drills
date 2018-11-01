36712c9fa62bbac31c36384994c256ecdf050ee1
Joaquin Fox <joaquin.fox@gmail.com>
2018-11-01 12:00:55 -0700

    finalize carousel

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 
 $(function(){
   $('.thumbnail').on('click', (event) =>{
-    const currentSrc = $(event.target).find('img').attr('src');
-    const currentAlt = $(event.target).find('img').attr('alt');
-
+    const currentSrc = $(event.currentTarget).find('img').attr('src');
+    const currentAlt = $(event.currentTarget).find('img').attr('alt');
+    event.preventDefault();
     $('.hero img').attr('src', currentSrc);
     $('.hero img').attr('alt', currentAlt);
   });
