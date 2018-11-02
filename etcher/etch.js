0b3e560533012595eb9cfac70825187518bd164b
Nikkie Mashian <nikmash@ucla.edu>
2018-11-01 21:43:56 -0700

    etcher

---
+++ etcher/etch.js
@@ -0,0 +1,50 @@
+// Add Event Listeners here:
+'use strict';
+/*eslint-env jquery*/
+//when DOM is ready: 
+$(function(){
+  createAndPlaceRows(8);
+  //cell doesnt exist yet bc JS automates it so we need event delegation
+  $('.grid').on('mouseenter', '.cell', (event)=>{
+    $(event.target).addClass('active');
+  });
+
+  $('.controls button').click(event=> createAndPlaceRows(8));
+});
+
+
+
+
+
+
+
+
+// ===============================
+// Code below is to automate grid creation
+// You can ignore this!
+// ===============================
+function createRow(cellCount) {
+  let row = '<div class="row">';
+  let str = '<div class="cell">&nbsp;</div>';
+  for (let i = 0; i < cellCount; i++) {
+    row += str;
+  }
+  row += '</div>';
+  return row;
+}
+
+function createRows(n) {
+  let rows = '';
+  for (let i = 0; i < n; i++) {
+    rows += createRow(n);
+  }
+  return rows;
+}
+
+function createAndPlaceRows(n) {
+  $('.cell').remove(); // Clear all current cells and listeners
+  const rows = createRows(n);
+  $('.grid').html(rows);
+  const cells = $('.cell');
+  cells.css({ height: cells.width() });
+}
